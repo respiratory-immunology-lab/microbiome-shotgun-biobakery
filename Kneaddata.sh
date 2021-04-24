fc2c0024f20953191e6df5d7a8be29958e741737
Celine Pattaroni <40220143+cpattaroni@users.noreply.github.com>
2021-04-24 11:32:25 +1000

    Update Kneaddata.sh

--- Kneaddata.sh
+++ Kneaddata.sh
@@ -2,7 +2,8 @@
 #SBATCH --job-name=KneadData
 #SBATCH --account=of33
 #SBATCH --time=3-00:00:00
-#SBATCH --ntasks=36
+#SBATCH --cores=4
+#SBATCH --ntasks=8
 #SBATCH --mem=200G
 
 # Load necessary environment and modules
@@ -16,8 +17,8 @@ mkdir kneaddata_output
 for f in rawfastq/*_R1.fastq.gz
 do
   Basename=${f%_R*}
-  echo kneaddata -t 6 --input ${Basename}_R1.fastq.gz --input ${Basename}_R2.fastq.gz \
+  echo kneaddata -t 8 -p 4 --input ${Basename}_R1.fastq.gz --input ${Basename}_R2.fastq.gz \
   --remove-intermediate-output --bypass-trf --output kneaddata_output \
   --trimmomatic /home/cpat0003/miniconda3/envs/biobakery3/bin/Trimmomatic-0.33 \
   -db /home/cpat0003/of33/Databases/shotgun/host/human/hg37dec_v0.1
-done | parallel -j 6
+done | parallel
